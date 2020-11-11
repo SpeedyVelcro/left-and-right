aedff1a128a383be91d8c1aa4e3fa4f497cb7cd3
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2020-11-11 03:09:18 +0000

    Added licenses to credits menu

--- UI/MainMenu/Credits/Credits.gd
+++ UI/MainMenu/Credits/Credits.gd
@@ -2,5 +2,36 @@
 
 extends Control
 
+var credits_file = "res://Credits.txt"
+var mit_file = "res://License/MIT.txt"
+var ofl_file = "res://License/OFL.txt"
+export(NodePath) var text_edit_path
+onready var text_edit = get_node(text_edit_path)
+
+func _ready():
+	display_text_file(credits_file)
+
 func _on_ButtonBack_pressed():
 	SceneTransition.instant("res://UI/MainMenu/MainMenu.tscn")
+
+func display_text_file(file_path):
+	var file = File.new()
+	file.open(file_path, File.READ)
+	var credits_text = ""
+	while true:
+		credits_text += file.get_line()
+		if file.eof_reached():
+			break
+		else:
+			credits_text += "\n"
+	file.close()
+	text_edit.set_text(credits_text)
+
+func _on_CreditsButton_pressed():
+	display_text_file(credits_file)
+
+func _on_MITButton_pressed():
+	display_text_file(mit_file)
+
+func _on_OFLButton_pressed():
+	display_text_file(ofl_file)
