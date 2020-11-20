75652b3e31518027bbf0383dbd939ab074eb5c21
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2020-11-20 14:37:38 +0000

    Hide quit button on HTML5

--- UI/MainMenu/MainMenu.gd
+++ UI/MainMenu/MainMenu.gd
@@ -2,6 +2,12 @@
 
 extends Control
 
+onready var quit_button = get_node("CenterContainer/Panel/VBoxContainer/HBoxContainer4/ButtonQuit")
+
+func _ready():
+	if OS.get_name() == "HTML5":
+		quit_button.set_visible(false)
+
 func _on_ButtonPlay_pressed():
 	SceneTransition.fade("res://Level/Instance/001.tscn", 0.2, 0.5)
 
